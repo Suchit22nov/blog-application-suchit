d53b9591a060fcf1db1ea10ba90ece662df5148d
suchit <suchitnov00@gmail.com>
2023-02-21 19:59:24 +0530

    added service ,dto, user,entities,

---
+++ src/main/java/com/codewithdurgesh/blog/repositories/UserRepo.java
@@ -0,0 +1,10 @@
+package com.codewithdurgesh.blog.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.codewithdurgesh.blog.entities.User;
+
+
+public interface UserRepo extends JpaRepository<User,Integer> {
+	
+}
---
+++ src/main/java/com/codewithdurgesh/blog/services/impl/UserServiceImpl.java
@@ -0,0 +1,60 @@
+package com.codewithdurgesh.blog.services.impl;
+
+import java.util.List;
+
+import com.codewithdurgesh.blog.entities.User;
+import com.codewithdurgesh.blog.payloads.UserDto;
+import com.codewithdurgesh.blog.repositories.UserRepo;
+import com.codewithdurgesh.blog.services.UserService;
+
+public class UserServiceImpl implements UserService {
+	private UserRepo userRepo;
+	@Override
+	public UserDto createUser(UserDto user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public UserDto updateUser(UserDto user, Integer userId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public UserDto getUserById(Integer userId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<UserDto> getAllUsers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deleteUser(Integer userId) {
+		// TODO Auto-generated method stub
+
+	}
+	public User dtoToUser(UserDto userDto) {
+		User user = new User();
+		user.setId(userDto.getId());
+		user.setName(userDto.getName());
+		user.setEmail(userDto.getEmail());
+		user.setAbout(userDto.getAbout());
+		user.setPassword(userDto.getPassword());
+		return user;
+	}
+	public UserDto UserToDto (User user) {
+		UserDto userDto = new UserDto();
+		userDto.setId(user.getId());
+		userDto.setName(user.getName());
+		userDto.setEmail(user.getEmail());
+		userDto.setAbout(user.getAbout());
+		userDto.setPassword(user.getPassword());
+		return userDto;
+	}
+
+}
