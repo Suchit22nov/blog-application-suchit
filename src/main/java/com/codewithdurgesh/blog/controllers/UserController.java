21c131746094ebaf06aa2d1b8bc5687e1dfddd08
suchit <suchitnov00@gmail.com>
2023-02-28 00:52:30 +0530

    added controller / created post and put user

---
+++ src/main/java/com/codewithdurgesh/blog/controllers/UserController.java
@@ -0,0 +1,37 @@
+package com.codewithdurgesh.blog.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.codewithdurgesh.blog.payloads.UserDto;
+import com.codewithdurgesh.blog.services.UserService;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+	@Autowired
+	private UserService userService;
+
+	// Post request create-user
+	@PostMapping("/")
+	public ResponseEntity<UserDto> createUser(@RequestBody UserDto userDto) {
+		UserDto createUserDto = this.userService.createUser(userDto);
+		return new ResponseEntity<>(createUserDto, HttpStatus.ACCEPTED);
+	}
+	//put -update user
+	@PutMapping("/{userId}")
+	public ResponseEntity<UserDto> updateUser(@RequestBody UserDto userDto,@PathVariable("userId")Integer uid){
+		UserDto userUpdated=this.userService.updateUser(userDto, uid);
+		return ResponseEntity.ok(userUpdated);
+		
+	}
+
+}
--- src/main/java/com/codewithdurgesh/blog/services/impl/UserServiceImpl.java
+++ src/main/java/com/codewithdurgesh/blog/services/impl/UserServiceImpl.java
@@ -1,43 +1,59 @@
 package com.codewithdurgesh.blog.services.impl;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.codewithdurgesh.blog.entities.User;
+import com.codewithdurgesh.blog.exceptions.ResourceNotFoundException;
 import com.codewithdurgesh.blog.payloads.UserDto;
 import com.codewithdurgesh.blog.repositories.UserRepo;
 import com.codewithdurgesh.blog.services.UserService;
-
+@Service
 public class UserServiceImpl implements UserService {
+	@Autowired
 	private UserRepo userRepo;
-	@Override
-	public UserDto createUser(UserDto user) {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
 	@Override
-	public UserDto updateUser(UserDto user, Integer userId) {
-		// TODO Auto-generated method stub
-		return null;
+	public UserDto createUser(UserDto userDto) {
+		User user = this.dtoToUser(userDto);
+		User saveduser = this.userRepo.save(user);
+		return this.UserToDto(saveduser);
+	}
+	
+	@Override
+	public UserDto updateUser(UserDto userDto, Integer userId) {
+		User user = this.userRepo.findById(userId).orElseThrow(()-> new ResourceNotFoundException("User","Id",userId));
+		user.setEmail(userDto.getEmail());
+		user.setName(userDto.getName());
+		user.setPassword(userDto.getPassword());
+		user.setAbout(userDto.getAbout());
+		User updateduser =this.userRepo.save(user);
+		UserDto userDto1 = this.UserToDto(updateduser);
+		return userDto1;
 	}
 
 	@Override
 	public UserDto getUserById(Integer userId) {
-		// TODO Auto-generated method stub
-		return null;
+		User user=this.userRepo.findById(userId).orElseThrow(()-> new ResourceNotFoundException("User","Id",userId));
+		return this.UserToDto(user);
 	}
 
 	@Override
 	public List<UserDto> getAllUsers() {
-		// TODO Auto-generated method stub
-		return null;
+		List<User> users = this.userRepo.findAll();
+		List<UserDto> userDtos =users.stream().map(user->this.UserToDto(user)).collect(Collectors.toList());
+		return userDtos;
 	}
 
 	@Override
 	public void deleteUser(Integer userId) {
-		// TODO Auto-generated method stub
-
+		User user = this.userRepo.findById(userId).orElseThrow(()-> new ResourceNotFoundException("User","Id",userId));
+		this.userRepo.delete(user);
 	}
+
 	public User dtoToUser(UserDto userDto) {
 		User user = new User();
 		user.setId(userDto.getId());
@@ -47,7 +63,8 @@ public class UserServiceImpl implements UserService {
 		user.setPassword(userDto.getPassword());
 		return user;
 	}
-	public UserDto UserToDto (User user) {
+
+	public UserDto UserToDto(User user) {
 		UserDto userDto = new UserDto();
 		userDto.setId(user.getId());
 		userDto.setName(user.getName());
