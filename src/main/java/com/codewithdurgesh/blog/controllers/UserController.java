cb23b8f14bfcba0e08b5203c4f68e9dd34f174d2
suchit <suchitnov00@gmail.com>
2023-03-11 18:58:33 +0530

    addming global exception handler

--- src/main/java/com/codewithdurgesh/blog/controllers/UserController.java
+++ src/main/java/com/codewithdurgesh/blog/controllers/UserController.java
@@ -1,8 +1,12 @@
 package com.codewithdurgesh.blog.controllers;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.codewithdurgesh.blog.payloads.ApiResponse;
 import com.codewithdurgesh.blog.payloads.UserDto;
 import com.codewithdurgesh.blog.services.UserService;
 
@@ -33,5 +38,30 @@ public class UserController {
 		return ResponseEntity.ok(userUpdated);
 		
 	}
-
+	//get all user 
+	@GetMapping("/")
+	public ResponseEntity<List<UserDto>> getAllUser(){
+		return ResponseEntity.ok(this.userService.getAllUsers());
+	}
+	// delete  method
+	@DeleteMapping("/{userId}")
+	public ResponseEntity<ApiResponse> deleteUser(@PathVariable("userId") Integer uid){
+		userService.deleteUser(uid);
+		return  new ResponseEntity<ApiResponse>(new ApiResponse( "User Deleted Successfully",true),HttpStatus.OK);
+	}
+	//get user by Id
+	@GetMapping("/{userId}")
+	public ResponseEntity<UserDto>getSingleUser(@PathVariable Integer userId){
+		UserDto uservalue=userService.getUserById(userId);
+		return ResponseEntity.ok(uservalue);
+		
+		
+	}
 }
+
+
+
+
+
+
+
