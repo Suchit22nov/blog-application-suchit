2964a6e72e116831260df525705f611ada45518c
suchit <suchitnov00@gmail.com>
2023-03-14 18:56:54 +0530

    added modelMapper

--- src/main/java/com/codewithdurgesh/blog/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/codewithdurgesh/blog/exceptions/GlobalExceptionHandler.java
@@ -1,5 +1,19 @@
 package com.codewithdurgesh.blog.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.codewithdurgesh.blog.payloads.ApiResponse;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<ApiResponse> responseNotFoundExceptionHandler(ResourceNotFoundException ex){
+		String message = ex.getMessage();
+		ApiResponse api = new ApiResponse(message , false);
+		return  new ResponseEntity<ApiResponse>(api,HttpStatus.NOT_FOUND);
+	}
 
 }
--- src/main/java/com/codewithdurgesh/blog/services/impl/UserServiceImpl.java
+++ src/main/java/com/codewithdurgesh/blog/services/impl/UserServiceImpl.java
@@ -3,18 +3,23 @@ package com.codewithdurgesh.blog.services.impl;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.codewithdurgesh.blog.entities.User;
 import com.codewithdurgesh.blog.exceptions.ResourceNotFoundException;
 import com.codewithdurgesh.blog.payloads.UserDto;
 import com.codewithdurgesh.blog.repositories.UserRepo;
 import com.codewithdurgesh.blog.services.UserService;
+
 @Service
 public class UserServiceImpl implements UserService {
 	@Autowired
 	private UserRepo userRepo;
+	
+	@Autowired
+	private ModelMapper modelMapper;
+
 
 	@Override
 	public UserDto createUser(UserDto userDto) {
@@ -22,40 +27,44 @@ public class UserServiceImpl implements UserService {
 		User saveduser = this.userRepo.save(user);
 		return this.UserToDto(saveduser);
 	}
-	
+
 	@Override
 	public UserDto updateUser(UserDto userDto, Integer userId) {
-		User user = this.userRepo.findById(userId).orElseThrow(()-> new ResourceNotFoundException("User","Id",userId));
+		User user = this.userRepo.findById(userId)
+				.orElseThrow(() -> new ResourceNotFoundException("User", "Id", userId));
 		user.setEmail(userDto.getEmail());
 		user.setName(userDto.getName());
 		user.setPassword(userDto.getPassword());
 		user.setAbout(userDto.getAbout());
-		User updateduser =this.userRepo.save(user);
+		User updateduser = this.userRepo.save(user);
 		UserDto userDto1 = this.UserToDto(updateduser);
 		return userDto1;
 	}
 
 	@Override
 	public UserDto getUserById(Integer userId) {
-		User user=this.userRepo.findById(userId).orElseThrow(()-> new ResourceNotFoundException("User","Id",userId));
+		User user = this.userRepo.findById(userId)
+				.orElseThrow(() -> new ResourceNotFoundException("User", "Id", userId));
 		return this.UserToDto(user);
 	}
 
 	@Override
 	public List<UserDto> getAllUsers() {
 		List<User> users = this.userRepo.findAll();
-		List<UserDto> userDtos =users.stream().map(user->this.UserToDto(user)).collect(Collectors.toList());
+		List<UserDto> userDtos = users.stream().map(user -> this.UserToDto(user)).collect(Collectors.toList());
 		return userDtos;
 	}
 
 	@Override
 	public void deleteUser(Integer userId) {
-		User user = this.userRepo.findById(userId).orElseThrow(()-> new ResourceNotFoundException("User","Id",userId));
+		User user = this.userRepo.findById(userId)
+				.orElseThrow(() -> new ResourceNotFoundException("User", "Id", userId));
 		this.userRepo.delete(user);
 	}
 
 	public User dtoToUser(UserDto userDto) {
-		User user = new User();
+
+		User user = this.modelMapper.map(userDto, User.class);
 		user.setId(userDto.getId());
 		user.setName(userDto.getName());
 		user.setEmail(userDto.getEmail());
